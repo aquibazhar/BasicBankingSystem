622939fe251e054e960da189e1c6c63a4871754e
aquibazhar <84010175+aquibazhar@users.noreply.github.com>
2021-05-11 14:48:13 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,60 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/Style1.css">
+    <link rel="stylesheet" type="text/css" href="css/navbarstyle.css">
+
+    <title>Basic Banking System</title>
+  </head>
+
+  <body>
+  <?php
+  include 'navbar.php';
+  ?>
+  
+      <div class="container-fluid">
+      <!-- Introduction section -->
+            <div class="row intro py-1">
+              <div class="col-sm-12 col-md">
+                <div class="heading text-center my-5">
+                  <h3>Welcome to</h3>
+                  <h1>SPARKS BANK</h1>
+                </div>
+              </div>
+              <div class="col-sm-12 col-md img text-center">
+                <img src="img/bank10.png" style="padding:15px;" height="500" width="500" class="img-fluid pt-2">
+              </div>
+            </div>
+
+      <!-- Activity section -->
+            <div class="row activity text-center">
+                  <div class="col-md act">
+                    <img src="img/user1.png" style="padding:15px;" height="245" width="245" class="img-fluid">
+                    <br>
+                    <a href="createuser.php"><button>Create a User</button></a>
+                  </div>
+                  <div class="col-md act">
+                    <img src="img/transfer1.png" style="padding:50px;" height="275" width="275" class="img-fluid">
+                    <br>
+                    <a href="transfermoney.php"><button>Make a Transaction</button></a>
+                  </div>
+                  <div class="col-md act">
+                    <img src="img/history1.png" style="padding:25px;" height="248" width="248" class="img-fluid">
+                    <br>
+                    <a href="transactionhistory.php"><button>Transaction History</button></a>
+                  </div>
+            </div>
+      </div>
+      <footer class="text-center mt-5 py-2">
+        <p>&copy 2020. Made by <b>Aquib Azhar</b> <br> The Sparks Foundation</p>
+      </footer>
+      <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+  </body>
+</html>
